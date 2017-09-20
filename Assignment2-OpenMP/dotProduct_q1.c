d5cefc23fbb2389990d15a10122158d0d0504d76
hprakash <hprakash@iu.edu>
2017-09-20 02:59:51 -0400

    Fixing synchronization problem by taking an array instead of a shared variable for dot_product

--- Assignment2-OpenMP/dotProduct_q1.c
+++ Assignment2-OpenMP/dotProduct_q1.c
@@ -2,22 +2,24 @@
  * Introduction to High Performance Computing 
  * Assignment 2
  * Question 1
- * Input buggy program
- * The problem with code is that "dot_prod" variable is not thread safe
+ * 
+ * The problem with code was that "dot_prod" variable is not thread safe
+ * Solution 1: for dot_product, instead of a single shared variable take an array  so that each
+ * thread write to separate array element without any synchronization problem
+ * Later add all the array element and that will be the dot_product
  * 
  * version 1
  */
- 
+
 #include <stdio.h>
 #include <omp.h>
 
 int main(){
 	const int N=100;
 	int i,k;
-	float a[N],b[N];
-	float dot_prod;
-	dot_prod=0.0;
-		
+	float a[N],b[N];	
+	float dot_prod[N];
+	
 	for(k=0;k<N;k++){
 		a[k]=3.0*k;
 		b[k]=1.8*k;
@@ -27,10 +29,12 @@ int main(){
 	{
 	#pragma omp for
 		for(i=0;i<N;i++){
-			dot_prod=dot_prod +a[i]*b[i];
+			dot_prod[i]=a[i]*b[i]; //each thread write its corresponding element without synchronization issue
 		}
 	}
-
-	printf("\nInner product of a[] and b[] = %f\n", dot_prod);
+	
+	float final_dot_prod=0.0; // to store the sum of all array elemnt 
+	for(i=0;i<N;i++){final_dot_prod+=dot_prod[i];}
+	printf("\nInner product of a[] and b[] = %f\n", final_dot_prod);
 	return 0;
 }
