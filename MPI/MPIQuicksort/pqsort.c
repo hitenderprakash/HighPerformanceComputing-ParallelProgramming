e41e0db593f04fd06a135d839595aa9360ea28f4
Hitender Prakash <hprakash@redbelly.soic.indiana.edu>
2017-10-28 22:39:51 -0400

    modified parallel quicksort program

--- MPI/MPIQuicksort/pqsort.c
+++ MPI/MPIQuicksort/pqsort.c
@@ -1,25 +1,33 @@
 /*
+ * Program to sort integers - Parallel Quicksort using MPI
  * Hitender Prakash
- * version 1
- * Program to sort an array in parallel using quick sort
+ * Approach: process with rank 0 initialize an array with rand()
+ * It divide the array in chunks and scatter within all processes
+ * each process sort its chunk using quick sort (sequential)
+ * Process with rank 0 gathers the chunks back to original array
+ * which is sorted in chunks
+ * Process with rank 0, now sequentially merge the sorted chunks to a new array
  * 
- */ 
+ * version 2: cosmetic changes and removing unwanted code
+ * Date: Oct 28, 2017
+ */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 
 #define min(a,b) (((a)<(b))?(a):(b))
 #define N 21
-typedef struct {
-	int init;
-	int end;
-} range;
+
+//swap two elements in array
 void swap(int *arr, int i, int j){
 	if(!arr || i==j){return;}
 	int temp=arr[i];
 	arr[i]=arr[j];
 	arr[j]=temp;
 }
+
+//display an array between two indices
 void displayArray(int *arr, int l, int h){
 	if(!arr){return;}
 	int i;
@@ -27,7 +35,8 @@ void displayArray(int *arr, int l, int h){
 	for(i=l;i<=h;i++){ printf("%d ",arr[i]);}
 	printf("\n");
 }
-
+//=====================================================================
+// classical quick sort implementation with partition() and quickSort()
 int partition(int *arr, int l, int h){
 	if(l>h){return -1;}
 	if(l==h){return l;}
@@ -49,38 +58,20 @@ void quickSort(int *arr, int l, int h){
 		quickSort(arr,p+1,h);
 	}
 }
-/*
-int *SequentialMerge(int *oldarr,int size, int numProc, range *procRange){
-	int *newarr=(int *)malloc(size*sizeof(int));
-	int itr=0;
-	while(itr<size){
-		int val=oldarr[0];
-		int incProc;
-		int i;
-		for(i=0;i<numProc;i++){
-			if(procRange[i].init <= procRange[i].end){
-				if(val >= oldarr[procRange[i].init]){
-					val=oldarr[procRange[i].init];
-					incProc=i;
-				}
-			}
-		}
-		procRange[incProc].init=procRange[incProc].init+1;
-		newarr[itr]=val;
-		itr++;
-	}	
-	return newarr;
-}*/
+//======================================================================
+
+//Method to merge the p sorted chunks into a single array 
 void SequentialMerge(int *oldarr,int *newarr,int chunk, int numProc){
 	int itr=0;
 	int val;
+	//array which keeps track of smallest element in every chunk
 	int *procRanges=(int*)malloc(numProc*sizeof(int));
 	int i;
 	for(i=0;i<numProc;i++){
 		procRanges[i]=i*chunk;
 	}
 	while(itr<(chunk*numProc)){
-		val=9999;
+		val=9999; //initialize val with a large number like INT_MAX
 		int incProc;
 		for(i=0;i<numProc;i++){
 			if(procRanges[i] < (i+1)*chunk){
@@ -90,21 +81,22 @@ void SequentialMerge(int *oldarr,int *newarr,int chunk, int numProc){
 				}
 			}
 		}
+		//increment the counter corresponding to chunk from where smallest element was taken
 		procRanges[incProc]=procRanges[incProc]+1;
+		//write smallest element to new sorted global array
 		newarr[itr]=val;
-
 		itr++;
 	}	
 }
 
 int main(int argc, char **argv)
 {
-	//create a random array 
-	int size=N;
+
+	int size=N; //number of elements to be created and sorted
 	int i=0;
-	int *origarr;
-	int *newarr;
-	int *sct;
+	int *origarr;//contains original array
+	int *newarr; //sorted return array
+	int *sct; //scattered chunk
 	
 	
 	MPI_Init(&argc,&argv);
@@ -125,64 +117,47 @@ int main(int argc, char **argv)
 		origarr=(int *)malloc((chunk*numProc)*sizeof(int));
 		for(i=0;i<size;i++){
 			origarr[i]=(int)rand()%20;
-			//newarr[i]=1;
 		}
+		//for scattering we want each process to have equal elements in chunk 
+		//if array can not be evenly divided we pad it with extra elements
+		//padding elements should be INT_MAX so that they remain in the end of the sorted array 
+		//and we can ignore them
 		for(i=size;i<(chunk*numProc);i++){
 			origarr[i]=9999;
-			//newarr[i]=1;
 		}
-		displayArray(origarr, 0, (chunk*numProc)-1);	
-		//displayArray(newarr, 0, (chunk*numProc)-1);
-			
+		printf("\n");
+		printf("\nThe elements to be sorted are:");
+		printf("\n=====================================================");
+		//displayArray(origarr, 0, (chunk*numProc)-1);
+		displayArray(origarr, 0, size-1);	//ignore padded elements			
 	}
 	
 	MPI_Scatter(origarr,chunk,MPI_INT,sct,chunk,MPI_INT,0,MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
-	//printf("\nMyRank: %d",rank);
+
+	//sort each chunk by quick sort sequentially
+	
 	quickSort(sct,0,chunk-1);
 	//displayArray(sct, 0, chunk-1);
+	
 	MPI_Gather(sct,chunk, MPI_INT,origarr, chunk, MPI_INT,0,MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
-	if(rank==0){
-		//newarr=(int *)malloc((chunk*numProc)*sizeof(int));
-		SequentialMerge(origarr,newarr,chunk, numProc);
-		displayArray(newarr, 0, (chunk*numProc)-1);
-	}
-	/*
-	//section to divide the task between processes
-	int chunk=0; //chunk size to be taken up by single process
-	if(size%numProc==0){
-		chunk=(size/numProc);
-	}
-	else{
-		chunk=(size/numProc)+1;
-	}
-  
-	//debug
-	//printf("\nChunk Size: %d",chunk);
-  
-  
-	//decide loop boundary to be executed by process 
-	int startLoop=chunk*rank;
-	int endLoop=min(startLoop+chunk-1,size-1);
-	//printf("\nI am rank: %d  I am picking from ind: %d to ind: %d",rank, startLoop, endLoop);
-	quickSort(origarr, startLoop, endLoop);
-	//printf("\nBang!!");
-	procRange[rank].init=startLoop;
-	procRange[rank].end=endLoop;
-	//debug info
-	printf("\nAfter Local Sort:");
-	displayArray(origarr, startLoop, endLoop);
 	
-	MPI_Barrier(MPI_COMM_WORLD);
-	printf("\nProc range Table info");
-	for(i=0;i<numProc;i++){
-		printf("\n %d %d", procRange[rank].init,procRange[rank].end);
+	if(rank==0){
+		//print original array after sorted in chunks
+		//displayArray(origarr, 0, (chunk*numProc)-1);
+		
+		//merge sequentially sorted chunks in a sorted array
+		SequentialMerge(origarr,newarr,chunk, numProc);
+		
+		//print final sorted array
+		//displayArray(newarr, 0, (chunk*numProc)-1);
+		printf("\nThe elements after sorting are:");
+		printf("\n=====================================================");
+		displayArray(newarr, 0, size-1); //ignore padded element
+		printf("\n\n");
 	}
-	//displayArray(origarr, 0, size-1);
-	//int *newarr = SequentialMerge(origarr,size, numProc, procRange);
-	//displayArray(newarr, 0, size-1);
-	*/
+
 	MPI_Finalize();
 	//free(origarr);
 	//free(newarr);
